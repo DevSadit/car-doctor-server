a032e89fce177c8e2c7053af57b9e72b835e1f67
DevSadit <dev.sadit@gmail.com>
2024-05-04 16:28:41 +0600

    services add

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
-app.use(express())
-
-
+app.use(cors());
+app.use(express());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.irm8dks.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -26,6 +24,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const servicesCollection = client.db("carsDoctor").collection("services");
+
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await servicesCollection.findOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -38,15 +52,9 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
-
-
-app.get(`/`, (req, res)=>{
-    res.send(`server is running`)
-})
-app.listen(port, ()=>{
-    console.log(`server is running on port ${port}`);
-})
+app.get(`/`, (req, res) => {
+  res.send(`server is running`);
+});
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
